fc7c1c99c75d884aba478f678260425258bda2ef
LoicZHU <zhu.loic@gmail.com>
2023-08-13 18:30:54 +0200

    refactor: services & middlewares

--- Startup.cs
+++ Startup.cs
@@ -9,12 +9,51 @@ public class Startup
 		this.Configuration = configuration;
 	}
 
-	// This method gets called by the runtime. Use this method to add serices to the container.
+	// This method gets called by the runtime. Use this method to add services to the container.
 	public void ConfigureServices(IServiceCollection services)
 	{
+		// Add services to the container.
 		//services.AddDbContext...
+
+		services.AddControllers(options => options.ReturnHttpNotAcceptable = true);
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		services.AddEndpointsApiExplorer().AddSwaggerGen();
 	}
 
 	// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-	public void Configure(WebApplication app, IWebHostEnvironment env) { }
+	public void Configure(WebApplication app, IWebHostEnvironment env)
+	{
+		this.UseMiddlewares(app);
+		this.AddEndpoints(app);
+	}
+
+	private void AddEndpoints(WebApplication app)
+	{
+		app.MapGet("/", () => "Hello World!");
+		app.MapControllers();
+	}
+
+	private void UseMiddlewares(WebApplication app)
+	{
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+		else
+		{
+			app.UseExceptionHandler(appBuilder =>
+			{
+				appBuilder.Run(async httpCtx =>
+				{
+					httpCtx.Response.StatusCode = 500;
+					await httpCtx.Response.WriteAsync("An unexpected fault happened. Try again later.");
+				});
+			});
+		}
+
+		app.UseHttpsRedirection();
+		app.UseAuthorization();
+	}
 }
